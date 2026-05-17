1ccb2b8fcd93159bfb1f0e89e49afa5dc3624b5b
Timur Murtazin <timur.murtazin0@gmail.com>
2026-05-17 21:12:40 +0300

    feat(web): add Razor Pages project

---
+++ src/SnapForge.Web/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+
+app.MapRazorPages();
+
+app.Run();
